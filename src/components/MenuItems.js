ccb469aaa8934fa452931fcd2d9c0379286f4fe3
meamrit777 <me.amrit777@gmail.com>
2022-02-11 19:11:06 +0545

    FEAT: Navbar completed(mobile view also) with dropdown

---
+++ src/components/MenuItems.js
@@ -0,0 +1,39 @@
+export const MenuItems = [
+    {
+      title: "Marketing",
+      path: "/marketing",
+      cName: "dropdown-link",
+    },
+    {
+      title: "Consulting",
+      path: "/consulting",
+      cName: "dropdown-link",
+    },
+    {
+      title: "Design",
+      path: "/design",
+      cName: "dropdown-link",
+    },
+    {
+      title: "Development",
+      path: "/development",
+      cName: "dropdown-link",
+    },
+  ];
+  
+  // const [show, setShow] = useState(true);
+  // const [scrollPos, setScrollPos] = useState(0);
+  // const componenDidMount = () => {
+  //   Window.addEventListener("scroll", handleScroll);
+  // };
+  // const componenWillUnMount = () => {
+  //   Window.removeEventListener("scroll", handleScroll);
+  // };
+  // handleScroll = () => {
+  //   console.log(document.body.getBoumdingClientRect());
+  //   this.setState({
+  //     scrollPos: document.body.getBoundingClientRect().top,
+  //     show: document.body.getBoundingClientRect().top > this.state.scrollPos,
+  //   });
+  // };
+  
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function SignUp() {
+  return <a href='https://www.instagram.com/invincible_amrit/' className='sign-up'>click anywhere to follow me</a>;
+}
