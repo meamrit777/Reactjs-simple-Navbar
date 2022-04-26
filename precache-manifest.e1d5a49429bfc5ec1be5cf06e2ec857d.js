fd604ae104381dc5e5d66f2ea4b163a49413fa63
Amrit Shrestha <me.amrit777@gmail.com>
2022-04-26 21:58:33 +0545

    Updates

---
+++ precache-manifest.e1d5a49429bfc5ec1be5cf06e2ec857d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770ee39a77967fe9588cd098934f36d9",
+    "url": "/Reactjs-simple-Navbar/index.html"
+  },
+  {
+    "revision": "6b48d40f7aa50f701cec",
+    "url": "/Reactjs-simple-Navbar/static/css/2.4e8a190c.chunk.css"
+  },
+  {
+    "revision": "c40f62c62ea65fafd5fd",
+    "url": "/Reactjs-simple-Navbar/static/css/main.b10d9153.chunk.css"
+  },
+  {
+    "revision": "6b48d40f7aa50f701cec",
+    "url": "/Reactjs-simple-Navbar/static/js/2.7dbff071.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/Reactjs-simple-Navbar/static/js/2.7dbff071.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40f62c62ea65fafd5fd",
+    "url": "/Reactjs-simple-Navbar/static/js/main.e7d4f6d1.chunk.js"
+  },
+  {
+    "revision": "34e93f60d910e47834c5",
+    "url": "/Reactjs-simple-Navbar/static/js/runtime-main.e6aed3d0.js"
+  },
+  {
+    "revision": "a31acefd998fee8e0483bff9c0705a96",
+    "url": "/Reactjs-simple-Navbar/static/media/3408.a31acefd.jpg"
+  },
+  {
+    "revision": "ed43a6085613dd2cf0f1ef50317c9173",
+    "url": "/Reactjs-simple-Navbar/static/media/img-1.ed43a608.jpg"
+  },
+  {
+    "revision": "b4fe647175cae877ccca2cee3925b981",
+    "url": "/Reactjs-simple-Navbar/static/media/img-2.b4fe6471.jpg"
+  },
+  {
+    "revision": "30897888bbb06a7381714cb25a82f445",
+    "url": "/Reactjs-simple-Navbar/static/media/img-3.30897888.jpg"
+  },
+  {
+    "revision": "9dba0bf5a3e017449097fb0f7e19040d",
+    "url": "/Reactjs-simple-Navbar/static/media/img-4.9dba0bf5.jpg"
+  },
+  {
+    "revision": "ad6fc37be343909fd9474c78ec93ba79",
+    "url": "/Reactjs-simple-Navbar/static/media/img-5.ad6fc37b.jpg"
+  },
+  {
+    "revision": "524b5bd38351f0e1a4efa331beb829f4",
+    "url": "/Reactjs-simple-Navbar/static/media/img-6.524b5bd3.jpg"
+  },
+  {
+    "revision": "2746ac0b887da3b27762e6cd5be016f3",
+    "url": "/Reactjs-simple-Navbar/static/media/img-7.2746ac0b.jpg"
+  },
+  {
+    "revision": "d48f838fd694843acf844bfe68df3dfa",
+    "url": "/Reactjs-simple-Navbar/static/media/img-8.d48f838f.jpg"
+  }
+]);
